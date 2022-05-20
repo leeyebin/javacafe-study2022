17748e37bb4ee9b090f8520bb87c31c1c56b789e
leeyebin <leeyebin007@gmail.com>
2022-05-21 03:12:44 +0900

    추가

--- JavaAPI_2022/src/practice/lang/string/StringPractice.java
+++ JavaAPI_2022/src/practice/lang/string/StringPractice.java
@@ -7,11 +7,16 @@ public class StringPractice {
 		String str2 = new String("abc");
 		
 		System.out.println(str1 == str2);
+		//System.out.println(str1.equals(str2));
+		
+		System.out.println();
+		
 		
 		String str3 = "abc";
 		String str4 = "abc";
 		
 		System.out.println(str3 == str4);
+		//System.out.println(str3.equals(str4));
 	}
 }
 
